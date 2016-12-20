5728dcaaad0a48ccbc6c53ada2cddc31aacab62e
lonely <shan.liu@msn.com>
2016-12-20 16:07:13 +0800

    ..

--- src/Bootstarp.php
+++ src/Bootstarp.php
@@ -1,6 +1,6 @@
 <?php
 namespace LSYS\MQ{
-	function __($string, array $values = NULL, $domain = "db")
+	function __($string, array $values = NULL, $domain = "default")
 	{
 		$i18n=\LSYS\I18n::instance(__DIR__."/I18n/");
 		return $i18n->__($string,  $values , $domain );
