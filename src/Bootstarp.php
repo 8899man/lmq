86077c26d26f832549949b89f6275d4499e2f7ee
lonely <shan.liu@msn.com>
2016-12-20 11:38:34 +0800

    .

--- src/Bootstarp.php
+++ src/Bootstarp.php
@@ -2,12 +2,12 @@
 namespace LSYS\MQ{
 	function __($string, array $values = NULL, $domain = "db")
 	{
-		$i18n=\LSYS\I18n::instance(dirname(__FILE__)."/I18n/");
+		$i18n=\LSYS\I18n::instance(__DIR__."/I18n/");
 		return $i18n->__($string,  $values , $domain );
 	}
 }
 namespace {
 	LSYS\Config\File::dirs(array(
-			dirname(__FILE__)."/config",
+			__DIR__."/config",
 	));
 }
